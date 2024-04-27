269f8d8a06d43cf6264524be19ad78d7a4ee5844
Mark-goga <markgala06@gmail.com>
2024-04-27 10:55:12 +0300

    xz_c

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -7,8 +7,12 @@ const textAreaEl = formEl.elements.message;
 const inputEl = formEl.elements.email;
 formEl.addEventListener('submit', (evn) => {
     evn.preventDefault();
+    const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+    formData.email = savedData.email;
+    formData.message = savedData.message;
     if (formData.email && formData.message) {
-        console.log(formData);
+        console.log("formData.message:", formData.message);
+        console.log("formData.email:", formData.email);
         localStorage.removeItem("feedback-form-state");
         formEl.reset();
     } else {
@@ -18,8 +22,8 @@ formEl.addEventListener('submit', (evn) => {
 
 formEl.addEventListener('input', (evn) => {
     const formElement = evn.currentTarget.elements;
-    formData.email = formElement.email.value;
-    formData.message = formElement.message.value;
+    formData.email = formElement.email.value.trim();
+    formData.message = formElement.message.value.trim();
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 })
 
